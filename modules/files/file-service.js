28e5cfd978a30ecd9526fbe34e3eb194413713a3
Savvas Stephanides <savvascyp@hotmail.com>
2022-06-29 15:02:53 +0100

    Update file-service.js

--- modules/files/file-service.js
+++ modules/files/file-service.js
@@ -10,28 +10,22 @@ export default function FileService() {
     }
 
     async function getAllFiles() {
-        console.log("Getting all files...")
         let response = await axios.get(`${process.env.API_BASE_URL}/files`, {
             headers: {
                 Cookie: `token=${token}`
             }
         })
         
-        console.log(`Got ${response.data.length} results`)
-
         return response.data
     }
 
     async function getFilesByCategory(categorySlug){
-        console.log(`Getting files for category ${categorySlug}`)
         let response = await axios.get(`${process.env.API_BASE_URL}/files/category/${categorySlug}`, {
             headers: {
                 Cookie: `token=${token}`
             }
         })
         
-        console.log(`Got ${response.data.length} results`)
-
         return response.data
     }
 }
